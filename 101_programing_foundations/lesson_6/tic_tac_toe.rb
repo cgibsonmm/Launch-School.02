6d9482ab071fe98fa3e2c9a7376f4cff7317ab49
cgibsonmm <cgibsonmm@gmail.com>
2018-03-23 11:06:06 -0400

    more work

---
+++ 101_programing_foundations/lesson_6/tic_tac_toe.rb
@@ -0,0 +1,31 @@
+# 1. Display the initial empty 3x3 board.
+# 2. Ask the user to mark a square.
+# 3. Computer marks a square.
+# 4. Display the updated board state.
+# 5. If winner, display winner.
+# 6. If board is full, display tie.
+# 7. If neither winner nor board is full, go to #2
+# 8. Play again?
+# 9. If yes, go to #1
+# 10. Good bye!
+
+def display_board(brd)
+  puts "****************************************"
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[1]}  |  #{brd[1]}"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[1]}  |  #{brd[1]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[1]}  |  #{brd[1]}"
+  puts "     |     |"
+  puts "****************************************"
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = ' '}
+  new_board
+end
