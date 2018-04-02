c2fb97cbfc37741fc22428dfa0397202849cb21b
cgibsonmm <cgibsonmm@gmail.com>
2018-04-02 19:18:36 -0400

    fixed folders and finished oop part 1 exercises

---
+++ Ruby_OOP/exercises/OOBasics/hello_sophie.rb
@@ -0,0 +1,33 @@
+# Using the code from the previous exercise, add a
+# parameter to #initialize that provides a name for the
+# Cat object. Use an instance variable to print a
+# greeting with the provided name. (You can remove the
+# code that displays I'm a cat!.)
+
+
+# Using the code from the previous exercise, move the greeting
+# from the #initialize method to an instance method named #greet
+# that prints a greeting when invoked.
+
+# V 1
+# class Cat
+#   def initialize(name)
+#     @name = name
+#     puts "Hello! My name is #{@name}!"
+#   end
+# end
+#
+# kitty = Cat.new('Sophie')
+
+class Cat
+  def initialize(name)
+    @name = name
+  end
+
+  def greet
+    puts "Hello! My name is #{@name}!"
+  end
+end
+
+kitty = Cat.new('Sophie')
+kitty.greet
