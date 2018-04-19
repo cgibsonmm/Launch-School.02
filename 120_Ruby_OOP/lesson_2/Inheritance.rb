cd2b4b197d232a90f73814364ddeca0ed134d8d5
cgibsonmm <cgibsonmm@gmail.com>
2018-04-19 09:56:00 -0400

    changed file name and finished inheritance lecture

---
+++ 120_Ruby_OOP/lesson_2/Inheritance.rb
@@ -0,0 +1,55 @@
+class Pet
+  def run
+    'running'
+  end
+
+  def jump
+    'jumping'
+  end
+end
+
+class Cat < Pet
+  def speak
+    'Meow'
+  end
+end
+
+class Dog < Pet
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+
+  def fetch
+    'fetching'
+  end
+end
+
+class Bulldog < Dog
+  def swim
+    "can't swim!"
+  end
+end
+
+pete = Pet.new
+kitty = Cat.new
+dave = Dog.new
+bud = Bulldog.new
+
+pete.run                # => "running!"
+pete.speak              # => NoMethodError
+
+kitty.run               # => "running!"
+kitty.speak             # => "meow!"
+kitty.fetch             # => NoMethodError
+
+dave.speak              # => "bark!"
+
+bud.run                 # => "running!"
+bud.swim                # => "can't swim!"
+
+# Cat < Pet
+# Bulldog < Dog < Pet
