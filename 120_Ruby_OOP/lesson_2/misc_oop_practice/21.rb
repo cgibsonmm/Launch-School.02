b86917fa4a92a67ce31caa57d48a0ed99927e05a
cgibsonmm <cgibsonmm@gmail.com>
2018-04-26 00:20:02 -0400

    worked on some oo practice

--- 120_Ruby_OOP/lesson_2/misc_oop_practice/21.rb
+++ 120_Ruby_OOP/lesson_2/misc_oop_practice/21.rb
@@ -3,14 +3,58 @@
 # to 21 without busting
 
 # rules house must hit at 15 or below,
+module DeckMath
+  CARD_VALUES = {'Ace' => [1, 11], 'Jack' => 10, 'Queen' => 10, 'King' => 10 }
 
-require 'pry'
+  def calculate_hand
+    current_total = 0
+    @hand.each do |card|
+      if card[0] == 'Ace' && current_total > 11
+        current_total += 1
+      elsif card[0] == 'Ace'
+        current_total += 11
+      elsif CARD_VALUES.include?(card[0])
+        current_total += 10
+      else
+        current_total += card[0].to_i
+      end
+    end
+    current_total
+  end
+
+  def self.to_value(card)
+    if card[0] == 'Ace'
+      11
+    elsif CARD_VALUES.include?(card[0])
+      10
+    else
+      card[0].to_i
+    end
+  end
+end
+
+module Actions
+  def options
+    %w[hit stay]
+  end
+
+  def hit
+    @hand << @dealer.deal_card
+  end
+
+  def stay
+    puts 'you have chosen to stay'
+  end
+end
 
 class Player
-  attr_accessor :hand, :name
+  include DeckMath
+  include Actions
+  attr_accessor :hand, :current_hand_total, :name, :actions
 
   def initialize
     @hand = []
+    @current_hand_total = 0
   end
 end
 
@@ -33,17 +77,23 @@ class Human < Player
 end
 
 class Dealer < Player
-  attr_accessor :game_deck
+  attr_accessor :game_deck, :visable_total, :hidden_card
   NAMES = %w[bobby red timmy jimmy]
 
   def initialize
     super
     @name = set_name
     @game_deck = Deck.new
+    @visable_total = 0
   end
 
   def set_name
     NAMES.sample
+  end
+
+  def visable_total
+    @hidden_card = hand[0]
+    current_hand_total - DeckMath.to_value(@hidden_card)
   end
 end
 
@@ -65,10 +115,16 @@ class Deck
   end
 end
 
+
+class Display
+end
+
 class GameEngine
   attr_accessor :human, :dealer
+  attr_reader :deck_tracker
 
   def initialize
+    system 'clear'
     @human = Human.new
     @dealer = Dealer.new
   end
@@ -85,9 +141,11 @@ class GameEngine
   end
 
   def display_player_hand
-    puts "Your staring hand is:
-          #{@human.hand[0][0]} of #{@human.hand[0][1]}
-          and a #{@human.hand[1][0]} of #{@human.hand[1][1]}"
+    puts "Cards are being delt..."
+    sleep(0.5)
+    puts "Your staring hand is:"
+    puts '-' * 20
+    puts "a #{display_card(@human.hand[0])} and a #{display_card(@human.hand[1])}"
   end
 
   def display_dealer_hand
@@ -95,18 +153,75 @@ class GameEngine
     puts "#{@dealer.name} is showing a #{@dealer.hand[1][0]} of #{@dealer.hand[1][1]}"
   end
 
+  def calculate_hands
+    @human.current_hand_total = @human.calculate_hand
+    @dealer.current_hand_total = @dealer.calculate_hand
+  end
+
+  def display_players_hand_total
+    puts "#{@human.name} you are showing a current hand total of #{@human.current_hand_total}."
+  end
+
+  def display_dealer_hand_total
+    puts "#{@dealer.name} the dealer, is showing a total of #{@dealer.visable_total}"
+  end
+
+  def display_delt_card(card)
+    puts "A #{display_card(card)} was drawn."
+  end
+
+  def display_card(card)
+    "#{card[0]} of #{card[1]}"
+  end
+
+  # Displays
   def setup_display
     display_names
     deal_starting_hand
     display_player_hand
     display_dealer_hand
-    display_current_visable_totals
   end
 
-  def display_current_visable_totals; end
+  def ask_human_choice
+    input = nil
+    loop do
+      puts 'hit or stay???'
+      input = gets.chomp.downcase
+      break if @human.options.include?(input)
+      puts 'Please enter valid choice'
+    end
+    if input == 'hit'
+      card = @dealer.game_deck.deal_card
+      display_delt_card(card)
+      @human.hand << card
+    end
+  end
+
+  def ask_dealer_choice
+    if @dealer.current_hand_total < 17
+      puts @dealer.current_hand_total
+      puts 'the dealer hits'
+      card = @dealer.game_deck.deal_card
+      puts display_card(card)
+      @dealer.hand << card
+    else
+      puts @dealer.current_hand_total
+      puts 'The dealer has chosen to stay'
+    end
+  end
 
   def play
     setup_display
+    calculate_hands
+    display_players_hand_total
+    display_dealer_hand_total
+
+    ask_human_choice
+    calculate_hands
+    display_players_hand_total
+
+    ask_dealer_choice
+
     # show hand
   end
 end
