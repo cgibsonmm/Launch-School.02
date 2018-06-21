a44b50e9cc3d6f5a4bd6dcbc070d94b00601d4ef
cgibsonmm <cgibsonmm@gmail.com>
2018-06-21 12:41:54 -0400

    push

---
+++ 120_Ruby_OOP/cat.rb
@@ -0,0 +1,15 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+end
+
+class Cat
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+  end
+end
+
+sara = Person.new("Sara")
+fluffy = Cat.new("Fluffy", sara)
--- 120_Ruby_OOP/store.rb
+++ 120_Ruby_OOP/store.rb
@@ -1,10 +1,32 @@
 class Store
+  attr_reader :items_in_stock
+  attr_writer :employee
+
   def initialize(owner, name)
     @created_at = Time.now
     @owner = owner
     @name = name
     @employes = []
+    @items_in_stock = []
   end
+
+  def add_item(item)
+    @items_in_stock << item
+  end
+
+  def show_items_instock
+    items_in_stock.each do |item|
+      puts item
+    end
+  end
+
+  def add_employee(employee)
+    @employes << employee
+  end
+
+  private
+
+  attr_writer :items_in_stock
 end
 
 class Employee
@@ -16,10 +38,20 @@ class Employee
 end
 
 class Item
-  def inintialize(name, sku, price, stock)
+  attr_reader :stock
+
+  def initialize(name, sku, price, stock)
     @name = name
     @sku = sku
     @price = price
     @stock = stock
   end
+
+  def stock_delivery(ammount)
+    self.stock += ammount
+  end
+
+  private
+
+  attr_writer :stock
 end
